a280d1887a8303836732b11c483c8543c50fbfa0
ajith0110 <santhan.ajith50@gmail.com>
2023-04-24 22:30:40 +0530

    jumb

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) ==true)
+        {
+            body.velocity = Vector2.up * 10;
+        }
+    }
+}
