6c2537c0624320eeb7a1ee1271b0be4c90f974bd
ajith0110 <santhan.ajith50@gmail.com>
2023-05-24 00:57:07 +0530

    visual preception v1

--- Assets/Scripts/Player/AutoJumb.cs
+++ Assets/Scripts/Player/AutoJumb.cs
@@ -12,23 +12,48 @@ public class AutoJumb : MonoBehaviour
    private Transform target;
 
 
-   JohnMovement john;
 
+   private JohnMovement yourScript;
    // Start is called before the first frame update
    void Start()
    {
-      target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-      john = new JohnMovement();
+
+      yourScript = GetComponent<JohnMovement>();
+      yourScript.jumpForce = 12;
    }
 
    // Update is called once per frame
-   void Update()
-   {
-      Debug.Log(Vector2.Distance(transform.position, target.position));
-      if (Vector2.Distance(transform.position, target.position) == 1)
-      {
-         john.Jump();
+   //    void Update()
+   //    {
+   //       Debug.Log(Vector2.Distance(transform.position, target.position));
+   //       if (Vector2.Distance(transform.position, target.position) == 1)
+   //       {
+   //          john.Jump();
 
+   //       }
+   //    }
+
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+
+      Debug.Log("Trigger Entered");
+
+      // Could use other.GetComponent<Player>() to see if the game object has a Player component
+      // Tags work too. Maybe some players have different script components?
+      if (other.tag == "Autojumb")
+      {
+
+         Debug.Log("Autojumb");
+         // Player entered, so move level
+         //  print("Switching Scene to " + sceneBuildIndex);
+         //  SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+         yourScript.Jump();
+
+         yourScript.jumpForce = 8;
       }
    }
+
+
+
 }
